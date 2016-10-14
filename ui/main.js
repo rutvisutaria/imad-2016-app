96b931f5b66724993153610e310a803d1a5e5ad9
rutvisutaria <rutvisutaria2@gmail.com>
2016-10-14 18:53:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML= 'ahhaaaa!!';
+var img = document.getElementById('r');
+img.onclick= function(){
+    img.style.marginleft= '50px';
+};
